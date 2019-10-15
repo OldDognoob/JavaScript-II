ba15ea911559565273959db2bca0f78d4152a38f
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-10-15 17:28:18 +0300

    array solving

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -88,7 +88,7 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
-function includes()
+
 
 
 /* STRETCH PROBLEM */
